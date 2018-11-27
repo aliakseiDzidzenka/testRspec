9cbe6cf3a5efa2b2302efc47e1b04b9b09a5af11
aliakseiDzidzenka <45169932+aliakseiDzidzenka@users.noreply.github.com>
2018-11-28 00:11:32 +0300

    Update rev.rb

--- lib/rev.rb
+++ lib/rev.rb
@@ -1,61 +1,58 @@
 # Definition for singly-linked list.
 class ListNode
-    attr_accessor :val, :nextN
-    def initialize(val)
-        if local_variables.length != 1
-        	raise ArgumentError.new("Wrong number of arguments")
-        end
-        @val = val
-        @nextN = nil
+  attr_accessor :val, :nextN
+  def initialize(val)
+    if local_variables.length != 1
+      raise ArgumentError.new("Wrong number of arguments")
     end
+    @val = val
+    @nextN = nil
+  end
 end
 
 def show(head)
-	if head.is_a?(ListNode) == false
-		"Argument is not a ListNode"
-	else
-		values = ""
-		tmp_el = head
-		values += "Your list values { "
-		while !tmp_el.nextN.nil?
-			values += tmp_el.val.to_s + " => "
-			tmp_el = tmp_el.nextN
-		end
-		values += tmp_el.val.to_s + " }"
-	end
+  if head.is_a?(ListNode) == false
+    "Argument is not a ListNode"
+  else
+    values = ""
+    tmp_el = head
+    values += "Your list values { "
+    while !tmp_el.nextN.nil?
+      values += tmp_el.val.to_s + " => "
+      tmp_el = tmp_el.nextN
+    end
+    values += tmp_el.val.to_s + " }"
+  end
 end
 def insert(head, value)
-		node = ListNode.new(value)
-		if head.nil?
-			head = node
-		else
-			tmp_el = head
-
-		while !tmp_el.nextN.nil?
-			tmp_el = tmp_el.nextN
-		end
-
-		tmp_el.nextN = node
-	end
+  node = ListNode.new(value)
+  if head.nil?
+    head = node
+  else
+    tmp_el = head
+    while !tmp_el.nextN.nil?
+      tmp_el = tmp_el.nextN
+    end
+    tmp_el.nextN = node
+   end
 end
 
 # @param {ListNode} head
 # @return {ListNode}
 def reverse_list(head)
-	if head.is_a?(ListNode) == false
-        raise ArgumentError.new("Only ListNode is allowed")
-    else
-		prev_node = nil
-		next_node = nil
-
-		while head
-		   	next_node = head.nextN
-		   	head.nextN = prev_node
-		   	prev_node = head
-		   	head = next_node
-		end 
-		return head = prev_node
-	end
+  if head.is_a?(ListNode) == false
+    raise ArgumentError.new("Only ListNode is allowed")
+  else
+    prev_node = nil
+    next_node = nil
+    while head
+      next_node = head.nextN
+      head.nextN = prev_node
+      prev_node = head
+      head = next_node
+    end 
+  return head = prev_node
+  end
 end
 
 # head = ListNode.new(1)
