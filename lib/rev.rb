834729f2c8514a8b10ae4c649c314cf657ea15d1
Aliaksei Dzidzenka <aliakseidzidzenka@gmail.com>
2018-11-27 23:50:22 +0300

    Ten tests are made

--- lib/rev.rb
+++ lib/rev.rb
@@ -2,21 +2,27 @@
 class ListNode
     attr_accessor :val, :nextN
     def initialize(val)
+        if local_variables.length != 1
+        	raise ArgumentError.new("Wrong number of arguments")
+        end
         @val = val
         @nextN = nil
     end
 end
 
-def display(head)
-
+def show(head)
+	if head.is_a?(ListNode) == false
+		"Argument is not a ListNode"
+	else
+		values = ""
 		tmp_el = head
-
+		values += "Your list values { "
 		while !tmp_el.nextN.nil?
-			p tmp_el.val
+			values += tmp_el.val.to_s + " => "
 			tmp_el = tmp_el.nextN
 		end
-		p tmp_el.val
-
+		values += tmp_el.val.to_s + " }"
+	end
 end
 def insert(head, value)
 		node = ListNode.new(value)
@@ -36,26 +42,30 @@ end
 # @param {ListNode} head
 # @return {ListNode}
 def reverse_list(head)
-   prev_node = nil
-   next_node = nil
+	if head.is_a?(ListNode) == false
+        raise ArgumentError.new("Only ListNode is allowed")
+    else
+		prev_node = nil
+		next_node = nil
 
-   while head
-   	  next_node = head.nextN
-   	  head.nextN = prev_node
-   	  prev_node = head
-   	  head = next_node
-	end 
-	return head = prev_node
+		while head
+		   	next_node = head.nextN
+		   	head.nextN = prev_node
+		   	prev_node = head
+		   	head = next_node
+		end 
+		return head = prev_node
+	end
 end
 
-head = ListNode.new(1)
-insert(head,2)
-insert(head,3)
-insert(head,4)
-insert(head,5)
+# head = ListNode.new(1)
+# insert(head,2)
+# insert(head,3)
+# insert(head,4)
+# insert(head,5)
 
-#display(head)
+# puts show(head)
 
-head = reverse_list(head)
+# head = reverse_list(head)
 
-#display(head)
+# puts show(head)
