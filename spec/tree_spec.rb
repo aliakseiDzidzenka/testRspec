535bf0d504964c238477d36b020781d8a7b8256e
Aliaksei Dzidzenka <aliakseidzidzenka@gmail.com>
2018-11-27 14:01:17 +0300

    different descriptions

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -19,18 +19,26 @@ RSpec.describe ListNode do
 	# 		expect(headNode.nextN.val).to eq(2)
 	# 	end
 	# end
+	value = 1
 	before(:each) do
-		@headNode = ListNode.new(1)
+		@headNode = ListNode.new(value)
 	end
 	describe "initialization" do
-		it "instance of #{described_class}, @val = value @nextN = nil" do
+		#let(:headNode){described_class.new(1)}
+		it "instance of #{described_class}, @val = #{value} @nextN = nil" do
 			expect(@headNode).to be_an_instance_of(ListNode)
-			expect(@headNode.val).to eq(1)
+			expect(@headNode.val).to eq(value)
 			expect(@headNode.nextN).to eq(nil)
 		end
-		it "inserts a new value" do
+	end
+	describe "insertion" do
+		it "inserts second and next elements" do
 			expect{insert(@headNode,2)}.to change{@headNode.nextN}.from(nil).to(described_class)
 			expect(@headNode.nextN.val).to eq(2)
+			expect{insert(@headNode,3)}.to change{@headNode.nextN.nextN}.from(nil).to(described_class)
+			expect(@headNode.nextN.nextN.val).to eq(3)
+			insert(@headNode,4)
+			expect(@headNode.nextN.nextN.nextN.val).to eq(4)
 		end
 	end
 		
