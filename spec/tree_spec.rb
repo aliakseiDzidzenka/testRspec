53953ab0d86aba2c4d3a39780b9d889f96975f30
aliakseiDzidzenka <45169932+aliakseiDzidzenka@users.noreply.github.com>
2018-11-30 16:17:19 +0300

    Update tree_spec.rb

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,85 +1,96 @@
-require "rev_linked_list"
+require 'test_data'
 
 RSpec.describe ListNode do 	
-  value = 1
-  before(:each) do
-    @headNode = ListNode.new(value)
 
-    @inNode = ListNode.new(1)
-    @inNode.nextN = ListNode.new(2)
-    @inNode.nextN.nextN = ListNode.new(3)
-    @inNode.nextN.nextN.nextN = ListNode.new(4)
-  end
-  describe "initialization" do
-    context "rigth parameters" do
-      it "instance of #{described_class}, @val = #{value} @nextN = nil" do
-        expect(@headNode).to be_an_instance_of(described_class)
-	expect(@headNode.val).to eq(value)
-	expect(@headNode.nextN).to eq(nil)
-      end
-    end
-    context "wrong parameters" do
-      it "raise error if not 1 parameter" do
-        expect{described_class.new}.to raise_error(ArgumentError)
-	expect{described_class.new(1, 1)}.to raise_error(ArgumentError)
-      end
-    end
-  end
+	value = reset_value
+	head_node = reset_head_node(value)
+	in_node = reset_in_node
 
-  describe "insertion" do
-    context "rigth parameters" do
-      it "inserts second and next nodes" do
-        expect{insert(@headNode,2)}.to change{@headNode.nextN}.from(nil).to(described_class)
-	expect(@headNode.nextN.val).to eq(2)
-	expect{insert(@headNode,3)}.to change{@headNode.nextN.nextN}.from(nil).to(described_class)
-	expect(@headNode.nextN.nextN.val).to eq(3)
-	insert(@headNode,4)
-	expect(@headNode.nextN.nextN.nextN.val).to eq(4)
-      end
-      it "insert to the end" do
-        expect{insert(@inNode.nextN,5)}.to change{@inNode.nextN.nextN.nextN.nextN}.from(nil).to(described_class)
-      end
-    end
-  end
-  describe "reverse" do
-    context "list reversion" do
-      it "returns #{described_class}" do
-        expect(reverse_list(@inNode)).to be_an_instance_of(described_class)
-      end
-      it "doesn't changes val, but truly reverse list" do
-        #добавляю метод только одному экземпляру, чтобы доказать настоящее инвертирование списка
-	class << @inNode.nextN
-	  def check_method
-            true
-          end
+	describe "#initialize" do
+		context "with right parameters" do
+			it "instance of #{described_class}" do
+				expect(head_node).to be_an_instance_of(described_class)
+			end
+			it "val = #{value}" do
+				expect(head_node.val).to eq(value)
+			end
+			it "next_n = nil" do
+				expect(head_node.next_n).to eq(nil)
+			end
+		end
+		context "with wrong parameters" do
+			it "raise error if no parameters" do
+				expect{described_class.new}.to raise_error(ArgumentError)
+			end
+			it "raise error if > 1 parameters" do
+				expect{described_class.new(1, 1)}.to raise_error(ArgumentError)
+			end
+		end
 	end
-	expect{@inNode = reverse_list(@inNode)}.to change{@inNode.val}.to(4)
-	expect(@inNode.nextN.nextN).to receive(:check_method).and_return(true)
-	@inNode.nextN.nextN.check_method
-      end
-    end
-    context "wrong arguments" do
-      it "raise error if not #{described_class}" do
-        expect{reverse_list 1}.to raise_error(ArgumentError)
-      end
-      it "raise error if not 1 parameter" do
-        expect{reverse_list}.to raise_error(ArgumentError)
-	expect{reverse_list(@inNode, @inNode)}.to raise_error(ArgumentError)
-      end
-    end
-  end
-  describe "show" do
-    context "right arguments count" do
-      it "returns a string" do
-        expect(show(@inNode)).to be_an_instance_of(String)
-	expect(show(value)).to be_an_instance_of(String)
-      end
-    end
-    context "wrong arguments count" do
-      it "raise error" do
-        expect{show}.to raise_error(ArgumentError)
-	expect{show(@inNode, @inNode)}.to raise_error(ArgumentError)
-      end
-    end
-  end
+
+	describe ".insert" do
+		context "with rigth parameters" do
+			it "inserts second and next nodes" do
+				head_node = reset_head_node(value)
+				expect{insert(head_node,2)}.to change{head_node.next_n}.from(nil).to(described_class)
+				expect(head_node.next_n.val).to eq(2)
+				expect{insert(head_node,3)}.to change{head_node.next_n.next_n}.from(nil).to(described_class)
+				expect(head_node.next_n.next_n.val).to eq(3)
+				insert(head_node,4)
+				expect(head_node.next_n.next_n.next_n.val).to eq(4)
+			end
+			it "insert to the end" do
+				in_node = reset_in_node
+				expect{insert(in_node.next_n,5)}.to change{in_node.next_n.next_n.next_n.next_n}.from(nil).to(described_class)
+			end
+		end
+	end
+	describe ".reverse_list" do
+		context "with right arguments" do
+			it "returns #{described_class}" do
+				expect(reverse_list(in_node)).to be_an_instance_of(described_class)
+			end
+			it "dosn't changes val, but truly reverse list" do
+				in_node = reset_in_node
+				class << in_node.next_n
+					def check_method
+						true
+					end
+				end
+				in_node = reverse_list(in_node)
+				expect(in_node.next_n.next_n.check_method).to eq(true)
+			end
+		end
+		context "with wrong arguments" do
+			it "raise error if not #{described_class}" do
+				expect{reverse_list 1}.to raise_error(ArgumentError)
+			end
+			it "raise error if no parameters" do
+				expect{reverse_list}.to raise_error(ArgumentError)
+			end
+			it "raise error if > 1 parameters" do
+				expect{reverse_list(in_node, in_node)}.to raise_error(ArgumentError)
+			end
+		end
+	end
+
+	describe ".show" do
+		context "with right arguments count" do
+			it "returns a string if #{described_class} given" do
+				in_node = reset_in_node
+				expect(show(in_node)).to be_an_instance_of(String)
+			end
+			it "returns a string if any argument given" do
+				expect(show(value)).to be_an_instance_of(String)
+			end
+		end
+		context "with wrong arguments count" do
+			it "raise error if no parameters" do
+				expect{show}.to raise_error(ArgumentError)
+			end
+			it "raise error if > 1 parameters" do
+				expect{show(in_node, in_node)}.to raise_error(ArgumentError)
+			end
+		end
+	end
 end
