c6d9364390e1b984ca4a0da00f5b1e837346eeb3
aliakseiDzidzenka <45169932+aliakseiDzidzenka@users.noreply.github.com>
2018-11-30 16:13:03 +0300

    Add files via upload

---
+++ lib/test_data.rb
@@ -0,0 +1,20 @@
+$LOAD_PATH.unshift(".")
+require 'rev'
+
+value = 1
+
+def reset_value
+	1
+end
+
+def reset_head_node(value)
+	ListNode.new(value)
+end
+
+def reset_in_node
+	in_node1 = ListNode.new(1)
+	in_node1.next_n = ListNode.new(2)
+	in_node1.next_n.next_n = ListNode.new(3)
+	in_node1.next_n.next_n.next_n = ListNode.new(4)
+	return in_node1
+end
